773699d021409d745f3b2b7a761680acc6970a90
Prathamesh Patil <prathamesh302008@gmail.com>
2026-08-24 17:07:52 +0530

    Fix static CSS files and deployment settings

--- cms/settings.py
+++ cms/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 
 # =========================================================
@@ -123,13 +125,11 @@ WSGI_APPLICATION = "cms.wsgi.application"
 # =========================================================
 
 DATABASES = {
-
-    "default": {
-
-        "ENGINE": "django.db.backends.sqlite3",
-
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
